89dab6c0ada6a1b8ff0c125b18aeabce67db9a10
amolij0311 <amolij0311@gmail.com>
2026-02-09 14:00:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function showLove() {
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      background:linear-gradient(to right,#ff9a9e,#fad0c4);
+      color:white;
+      text-align:center;
+      padding:20px;
+    ">
+      <img src="https://media.tenor.com/GX3G0G5Vq3IAAAAi/love-heart.gif" width="180">
+      <br>
+      <div style="
+        font-family: 'Poppins', sans-serif;
+        font-size: 30px;
+        font-weight: 600;
+      ">
+        YAYYYY 🥰💖<br>
+        HAPPY VALENTINE'S DAY ❤️✨
+      </div>
+    </div>
+  `;
+}
